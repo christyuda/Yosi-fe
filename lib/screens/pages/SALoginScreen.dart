3520b2bf2b7b8afb061b79d1d8c91ea7ae56dec1
christyuda <chrisyuda@ulbi.ac.id>
2024-02-17 13:38:19 +0700

    Update login screen UI

--- lib/screens/pages/SALoginScreen.dart
+++ lib/screens/pages/SALoginScreen.dart
@@ -16,10 +16,19 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Image.asset(
+            //   'assets/images/login_logo.png', // Ubah sesuai dengan lokasi logo Anda
+            //   width: 150,
+            //   height: 150,
+            //   fit: BoxFit.contain,
+            // ),
+            SizedBox(height: 20),
             TextField(
               onChanged: (value) => _loginController.setUsername(value),
               decoration: InputDecoration(
                 labelText: 'Username',
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
               ),
             ),
             SizedBox(height: 20),
@@ -27,6 +36,8 @@ class LoginScreen extends StatelessWidget {
               onChanged: (value) => _loginController.setPassword(value),
               decoration: InputDecoration(
                 labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
               ),
               obscureText: true,
             ),
@@ -40,13 +51,23 @@ class LoginScreen extends StatelessWidget {
                       child: Text(value),
                     );
                   }).toList(),
+                  decoration: InputDecoration(
+                    labelText: 'Role',
+                    border: OutlineInputBorder(),
+                  ),
                 )),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () async {
                 _loginController.login();
               },
-              child: Text('Login'),
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(fontSize: 16),
+              ),
             ),
           ],
         ),
